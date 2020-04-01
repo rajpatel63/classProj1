95b9c8675fe71491fa242daf6789dbac134e3662
rajpatel63 <54943599+rajpatel63@users.noreply.github.com>
2020-04-01 18:54:10 -0500

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -14,4 +14,8 @@ checkAccount("none");
 </head>
 <body>
 This is the welcome page for our project. This is Raj's Copy.
+If you don't have an account, you can create one <a href='createAccount.php'> here </a>. <br/>
+
+Login Form goes here
+
 </body>

c4d38a8b9a4695da07e7528cb864a742b09c3ca8
rajpatel63 <54943599+rajpatel63@users.noreply.github.com>
2020-04-01 18:51:51 -0500

    Update functions.php

--- library/functions.php
+++ library/functions.php
@@ -5,6 +5,22 @@ function checkAccount($zone)
     // zone is either 'user' or 'admin', anything else is considered 
     // 'none' or publicly accessible
 
+    /// we start the session in checkAccount to make sure it's called.
+    session_start();
+}
+
+function getDBConnection()
+{
+    $user = "rpatel63";
+    $conn = mysqli_connect("localhost",$user,$user,$user);
+
+    // Check connection and shutdown if broken
+    if (mysqli_connect_errno()) {
+	die("<b>Failed to connect to MySQL: " . mysqli_connect_error() . "</b>");
+    }
+
+    return $conn;
 }
 
 ?>
+

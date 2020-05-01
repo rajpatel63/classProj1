e8cb634e981535c730493372e4ca3d06c865f7f1
rajpatel63 <54943599+rajpatel63@users.noreply.github.com>
2020-05-01 13:50:11 -0500

    Create page_template.php

---
+++ template/page_template.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>CSC155 001DR Survey Thing</title>
+<link rel="stylesheet" type="text/css" href="library/styles.css">
+<?php
+
+require ('library/functions.php');
+
+// depending on the zone, call one of
+checkAccount("none");
+//checkAccount("user");
+//checkAccount("admin");
+
+// get connection object
+$conn = getDBConnection();
+
+?>
+</head>
+<body>
+
+</body>
